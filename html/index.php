1ef4f72dda8b4eb0c1271fedfced9415cab042b6
velip <anders_teehee@hotmail.no>
2022-08-31 15:00:48 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -4,8 +4,8 @@ $txtFile = fopen("../key.txt", "r") or die ("Unable to open file!");
 $key = fread($txtFile,filesize("../key.txt"));
 fclose($txtFile);
 
-if($_GET['c'] == $key && isset($_GET['c'])) {
-    echo "Access!";
-} else {
+if($_GET['c'] != $key) {
     exit();
 }
+
+//Code goes here
